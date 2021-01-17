51150bc0bf24c74ca3de0ed99d84bc0866103735
pzuanigh-code <76559492+pzuanigh-code@users.noreply.github.com>
2021-01-17 17:48:53 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! From PZU demo'));
+app.get('/', (req, res) => res.send('Hello World! From Choupinou to Doudou via Amazon ElasticBeanStalk'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
